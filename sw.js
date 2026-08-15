b0fc7a02fbea1f241fbfd7c210c1fceb5b4e825a
weirdmiestro-stormkeeper <weirdmiestro@gmail.com>
2026-08-15 14:24:10 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE="stormkeeper-v6";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./css/main.css","./js/app.js","./manifest.webmanifest"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
